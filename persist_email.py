e6fd0aee18f1ed914f5ef4b43076cc5d4f4d752e
Adam Hsu <adamhsu@Adams-MacBook-Pro-2.local>
2019-08-27 11:42:49 -0700

    minor fix

--- persist_email.py
+++ persist_email.py
@@ -5,6 +5,7 @@ COMPANY_ID = "27d80a54-f0ab-4f19-8a69-9e2f37b06071"
 ENV = "demo"
 URL = "https://juvenal.{}.globality.io/api/v2/resource_tag".format(ENV)
 
+
 def main():
     payload = {
         "resourceId": COMPANY_ID,
