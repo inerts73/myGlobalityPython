20bcf69ca80d612d52303e7c694152e2abcc6859
Adam Hsu <adamhsu@Adams-MacBook-Pro-2.local>
2019-09-13 15:18:53 -0700

    Minor fix

--- generate_chars.py
+++ generate_chars.py
@@ -1,3 +1,5 @@
 # Generate 5000 chars (does not count \n)
-for i in range(50):
-  print("{0:02d}_100CharsPerLine_{1}".format(i+1, "Globality"*9))
+with open("generate_chars.txt", "w") as fp:
+    for i in range(50):
+        print("{0:02d}_100CharsPerLine_{1}".format(i+1, "Globality"*9))
+        fp.write("{0:02d}_100CharsPerLine_{1}\n".format(i+1, "Globality"*9))
